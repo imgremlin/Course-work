bd89eb6af7892ad5e3641fd9ac0f1ff1df84cef1
Alex <40468037+imgremlin@users.noreply.github.com>
2019-04-09 11:18:43 +0300

    Update Menu.c

--- Menu.c
+++ Menu.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-	int c,k;
+	int c,k1,k2,k3;
 	printf(	"1.Red line\n"
 		    "2.Green line\n"
 		    "3.Blue line\n"
@@ -10,30 +10,6 @@ int main() {
 	printf("\n");
 	switch (c)
 	{
-		case 3: printf( "1. Heroiv Dnipra\n"
-						"2.Minska\n"
-						"3.Obolon\n"
-						"4.Pochaina\n"
-						"5.Tarasa Shevchenka\n"
-						"6.Kontraktova Ploshcha\n"
-						"7.Poshtova Ploshcha\n"
-						"8.Maidan Nezalezhnosti\n"
-						"9.Ploshcha Lva Tolstoho\n"
-						"9.Olimpiiska\n"
-						"10.Palats Ukrayina\n"
-						"11.Lybidskav\n"
-						"12.Demiivska\n"
-						"13.Holosiivska\n"
-						"14.Vasylkivska\n"
-						"15.Vystavkovyi Tsentr\n"
-						"16.Ipodrom\n"
-						"17.Teremky\n"
-						);
-				printf("Choose the station: ");
-				scanf("%i", &k);
-				printf("\n");
-	        	break;
-	    
 				case 1: printf( "1.Akademmistechko\n"
 								"2.Zhytomyrska\n"
 								"3.Sviatoshyn\n"
@@ -54,10 +30,9 @@ int main() {
 								"17.Lisova\n"
 						);
 				printf("Choose the station: ");
-				scanf("%i", &k);
+				scanf("%i", &k1);
 				printf("\n");
 	        	break;
-	        	
 	        	
 	        	case 2: printf( "1.Syrets\n"
 								"2.Dorohozhychi\n"
@@ -77,94 +52,31 @@ int main() {
 								"15.Chervony Khutir\n"
 						);
 				printf("Choose the station: ");
-				scanf("%i", &k);
+				scanf("%i", &k2);
 				printf("\n");
 	        	break;
 	        	
-		default: printf("Input correct number");
-	}
-	return 0;
-}
-#include <stdio.h>
-int main() {
-	int c,k;
-	printf(	"1.Red line\n"
-		    "2.Green line\n"
-		    "3.Blue line\n"
-				);
-	printf("Choose the line: ");
-	scanf("%i", &c);
-	printf("\n");
-	switch (c)
-	{
-		case 3: printf( "1. Heroiv Dnipra\n"
-						"2.Minska\n"
-						"3.Obolon\n"
-						"4.Pochaina\n"
-						"5.Tarasa Shevchenka\n"
-						"6.Kontraktova Ploshcha\n"
-						"7.Poshtova Ploshcha\n"
-						"8.Maidan Nezalezhnosti\n"
-						"9.Ploshcha Lva Tolstoho\n"
-						"9.Olimpiiska\n"
-						"10.Palats Ukrayina\n"
-						"11.Lybidskav\n"
-						"12.Demiivska\n"
-						"13.Holosiivska\n"
-						"14.Vasylkivska\n"
-						"15.Vystavkovyi Tsentr\n"
-						"16.Ipodrom\n"
-						"17.Teremky\n"
+	        	case 3: printf( "1. Heroiv Dnipra\n"
+								"2.Minska\n"
+								"3.Obolon\n"
+								"4.Pochaina\n"
+								"5.Tarasa Shevchenka\n"
+								"6.Kontraktova Ploshcha\n"
+								"7.Poshtova Ploshcha\n"
+								"8.Maidan Nezalezhnosti\n"
+								"9.Ploshcha Lva Tolstoho\n"
+								"9.Olimpiiska\n"
+								"10.Palats Ukrayina\n"
+								"11.Lybidskav\n"
+								"12.Demiivska\n"
+								"13.Holosiivska\n"
+								"14.Vasylkivska\n"
+								"15.Vystavkovyi Tsentr\n"
+								"16.Ipodrom\n"
+								"17.Teremky\n"
 						);
 				printf("Choose the station: ");
-				scanf("%i", &k);
-				printf("\n");
-	        	break;
-	    
-				case 1: printf( "1.Akademmistechko\n"
-								"2.Zhytomyrska\n"
-								"3.Sviatoshyn\n"
-								"4.Nyvky\n"
-								"5.Beresteiska\n"
-								"6.Shuliavska\n"
-								"7.Politekhnichnyi Instytut\n"
-								"8.Vokzalna\n"
-								"9.Universytet\n"
-								"9.Teatralna\n"
-								"10.Khreshchatyk\n"
-								"11.Arsenalna\n"
-								"12.Dnipro\n"
-								"13.Hidropark\n"
-								"14.Livoberezhna\n"
-								"15.Darnytsia\n"
-								"16.Chernihivska\n"
-								"17.Lisova\n"
-						);
-				printf("Choose the station: ");
-				scanf("%i", &k);
-				printf("\n");
-	        	break;
-	        	
-	        	
-	        	case 2: printf( "1.Syrets\n"
-								"2.Dorohozhychi\n"
-								"3.Lukianivska\n"
-								"4.Zoloti Vorota\n"
-								"5.Palats Sportu \n"
-								"6.Klovska\n"
-								"7.Pecherska\n"
-								"8.Druzhby Narodiv\n"
-								"9.Vydubychi\n"
-								"9.Slavutych\n"
-								"10.Osokorky\n"
-								"11.Pozniaky\n"
-								"12.Kharkivska\n"
-								"13.Vyrlytsia\n"
-								"14.Boryspilska\n"
-								"15.Chervony Khutir\n"
-						);
-				printf("Choose the station: ");
-				scanf("%i", &k);
+				scanf("%i", &k3);
 				printf("\n");
 	        	break;
 	        	
