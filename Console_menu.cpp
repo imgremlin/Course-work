0f56f148c2fd13f202bd2a90523a0031dff1ae2c
Alex <40468037+imgremlin@users.noreply.github.com>
2019-04-23 22:45:51 +0300

    Update Console_menu.cpp

--- Console_menu.cpp
+++ Console_menu.cpp
@@ -13,11 +13,11 @@
 
 using namespace std;
 
-int Choosestation_red(int switcher2)
+int Choosestation_red(int switcher)
 {
-  int k;
+  	int k;
     system("cls");
-    switch (switcher2)
+    switch (switcher)
     {
     case 1:
         cout << "\n\n\n                    <<  Akademmistechko!  >>\n\n                    Zhytomyrska\n\n                    Sviatoshyn\n\n                    Nyvky\n\n                    Beresteiska\n\n                    Shuliavska\n\n                    Politekhnichnyi Instytut\n\n                    Vokzalna\n\n                    Universytet\n\n                    Teatralna\n\n                    Khreshchatyk\n\n                    Arsenalna\n\n                    Dnipro\n\n                    Hidropark\n\n                    Livoberezhna\n\n                    Darnytsia\n\n                    Chernihivska\n\n                   Lisova\n\n";
@@ -78,37 +78,48 @@ int Choosestation_red(int switcher2)
     if (choice == 224)
         choice = getch();
     if (choice == UP)
-        if (switcher2 != 1)
-            Choosestation_red(switcher2 - 1);
+        if (switcher != 1)
+            Choosestation_red(switcher - 1);
         else
             Choosestation_red(18);
     if (choice == DOWN)
-        if (switcher2 != 18)
-            Choosestation_red(switcher2 + 1);
+        if (switcher != 18)
+            Choosestation_red(switcher + 1);
         else
             Choosestation_red(1);
     if (choice == ENTER || choice == SPACE)
     {
-        if (switcher2 == 1)
-        	k = 1;
-        	return k;
-        	printf("%d", k);
-  			
-        //if (switcher1 == 2)
-           // Help(0);
-        //if (switcher1 == 3)
-            //TopChart();
+        if (switcher == 1) k=1;	
+        if (switcher == 2) k=2;
+        if (switcher == 3) k=3;
+        if (switcher == 4) k=4;
+        if (switcher == 5) k=5;
+        if (switcher == 6) k=6;
+        if (switcher == 7) k=7;
+        if (switcher == 8) k=8;
+        if (switcher == 9) k=9;
+        if (switcher == 10) k=10;
+        if (switcher == 11) k=11;
+        if (switcher == 12) k=12;
+        if (switcher == 13) k=13;
+        if (switcher == 14) k=14;
+        if (switcher == 15) k=15;
+        if (switcher == 16) k=16;
+        if (switcher == 17) k=17;
+        if (switcher == 18) k=18;
+        
+      	return k;
     
 }}
 
 
 
 
-int Choosestation_green(int switcher3)
+int Choosestation_green(int switcher)
 {
-  
+  	int k;
     system("cls");
-    switch (switcher3)
+    switch (switcher)
     {
     case 1:
         cout << "\n\n\n                    <<  Syrets!  >>\n\n                    Dorohozhychi\n\n                    Lukianivska\n\n                    Zoloti Vorota\n\n                    Palats Sportu\n\n                    Klovska\n\n                    Pecherska\n\n                    Druzhby Narodiv\n\n                    Vydubychi\n\n                    Slavutych\n\n                    Osokorky\n\n                    Pozniaky\n\n                    Kharkivska\n\n                    Vyrlytsia\n\n                    Boryspilska\n\n                    Chervony Khutir\n\n";
@@ -164,32 +175,43 @@ int Choosestation_green(int switcher3)
     if (choice == 224)
         choice = getch();
     if (choice == UP)
-        if (switcher3 != 1)
-            Choosestation_green(switcher3 - 1);
+        if (switcher != 1)
+            Choosestation_green(switcher - 1);
         else
             Choosestation_green(16);
     if (choice == DOWN)
-        if (switcher3 != 16)
-            Choosestation_green(switcher3 + 1);
+        if (switcher != 16)
+            Choosestation_green(switcher + 1);
         else
             Choosestation_green(1);
     if (choice == ENTER || choice == SPACE)
     {
-        if (switcher3 == 1){
-        	//Chooseline(1);
-  			};
-        //if (switcher1 == 2)
-           // Help(0);
-        //if (switcher1 == 3)
-            //TopChart();
+        if (switcher == 1) k=1;	
+        if (switcher == 2) k=2;
+        if (switcher == 3) k=3;
+        if (switcher == 4) k=4;
+        if (switcher == 5) k=5;
+        if (switcher == 6) k=6;
+        if (switcher == 7) k=7;
+        if (switcher == 8) k=8;
+        if (switcher == 9) k=9;
+        if (switcher == 10) k=10;
+        if (switcher == 11) k=11;
+        if (switcher == 12) k=12;
+        if (switcher == 13) k=13;
+        if (switcher == 14) k=14;
+        if (switcher == 15) k=15;
+        if (switcher == 16) k=16;
+        k+=18;
+      	return k;
     
 }}
 
-int Choosestation_blue(int switcher2)
+int Choosestation_blue(int switcher)
 {
-  
+  	int k;
     system("cls");
-    switch (switcher2)
+    switch (switcher)
     {
     case 1:
         cout << "\n\n\n                    <<  Heroiv Dnipra!  >>\n\n                    Minska\n\n                    Obolon\n\n                    Pochaina\n\n                    Tarasa Shevchenka\n\n                    Kontraktova Ploshcha\n\n                    Poshtova Ploshcha\n\n                    Maidan Nezalezhnosti\n\n                    Ploshcha Lva Tolstoho\n\n                    Olimpiiska\n\n                    Palats Ukrayina\n\n                    Lybidska\n\n                    Demiivska\n\n                    Holosiivska\n\n                    Vasylkivska\n\n                    Vystavkovyi Tsentr\n\n                    Ipodrom\n\n                    Teremky\n\n";
@@ -250,30 +272,43 @@ int Choosestation_blue(int switcher2)
     if (choice == 224)
         choice = getch();
     if (choice == UP)
-        if (switcher2 != 1)
-            Choosestation_blue(switcher2 - 1);
+        if (switcher != 1)
+            Choosestation_blue(switcher - 1);
         else
             Choosestation_blue(18);
     if (choice == DOWN)
-        if (switcher2 != 18)
-            Choosestation_blue(switcher2 + 1);
+        if (switcher != 18)
+            Choosestation_blue(switcher + 1);
         else
             Choosestation_blue(1);
     if (choice == ENTER || choice == SPACE)
     {
-        if (switcher2 == 1){
-        	//Chooseline(1);
-  			};
-        //if (switcher1 == 2)
-           // Help(0);
-        //if (switcher1 == 3)
-            //TopChart();
+        if (switcher == 1) k=1;	
+        if (switcher == 2) k=2;
+        if (switcher == 3) k=3;
+        if (switcher == 4) k=4;
+        if (switcher == 5) k=5;
+        if (switcher == 6) k=6;
+        if (switcher == 7) k=7;
+        if (switcher == 8) k=8;
+        if (switcher == 9) k=9;
+        if (switcher == 10) k=10;
+        if (switcher == 11) k=11;
+        if (switcher == 12) k=12;
+        if (switcher == 13) k=13;
+        if (switcher == 14) k=14;
+        if (switcher == 15) k=15;
+        if (switcher == 16) k=16;
+        if (switcher == 17) k=17;
+        if (switcher == 18) k=18;
+        k+=34;
+      	return k;
     
 }}
 
 int Chooseline(int switcher1)
 {
-  
+   int l;
     system("cls");
     switch (switcher1)
     {
@@ -306,19 +341,20 @@ int Chooseline(int switcher1)
     if (choice == ENTER || choice == SPACE)
     {
         if (switcher1 == 1)
-        	Choosestation_red(1);
+        	l = Choosestation_red(1);
         if (switcher1 == 2)
-           Choosestation_green(1);
+           l = Choosestation_green(1);
         if (switcher1 == 3)
-            Choosestation_blue(1);
+            l = Choosestation_blue(1);
+        return l;    
+        
     
 }}
 
 
 
 int StartMenu(int switcher)
-{
-  //load_scores();
+{	int y;
     system("cls");
     switch (switcher)
     {
@@ -351,13 +387,15 @@ int StartMenu(int switcher)
     if (choice == ENTER || choice == SPACE)
     {
         if (switcher == 1){
-        	Chooseline(1);
+        	y = Chooseline(1);
         
   };
         if (switcher == 2)
         if (switcher == 3)
         if (switcher == 4)
             exit(0);
+        //int y = Chooseline(1);
+		cout<<y;    
     }
 }
 
@@ -368,9 +406,7 @@ int StartMenu(int switcher)
 int main()
 {
 	int x;
-	StartMenu(1);
-	x = Choosestation_red(1);
-	cout<<x;
+	x = StartMenu(1);
 	return 0;
 	
 }
