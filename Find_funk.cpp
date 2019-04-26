fdee1c4febe4342cda567944321feaa75a9b722e
Alex <40468037+imgremlin@users.noreply.github.com>
2019-04-26 23:13:09 +0300

    Update Find_funk.cpp

--- Find_funk.cpp
+++ Find_funk.cpp
@@ -28,16 +28,14 @@ char stations[52][30] = {"AKADEMMISTECHKO", "ZHYTOMYRSKA", "SVIATOSHYN", "NYVKY"
 							 "HOLOSIIVSKA", "VASYLKIVSKA", "VYSTAVKOVYI TSENTR", "IPODROM", "TEREMKY"};
 
 
-
-
-int Choosestation_enter_menu(int switcher, int counter, char ( &dyn_stations )[10][30])
+int Choosestation_enter_menu(int switcher, int counter, char ( &dyn_stations )[15][30])
 {
 	
     int k, j, l, lenth;
     system("cls");
     
     cout<<endl<<endl;
-    for (int i=0; i<3; i++)
+    for (int i=0; i<counter; i++)
     {	
     	lenth = 23 - strlen(dyn_stations[i])/2;
     	if (i==switcher-1)
@@ -58,9 +56,9 @@ int Choosestation_enter_menu(int switcher, int counter, char ( &dyn_stations )[1
         if (switcher != 1)
             Choosestation_enter_menu(switcher - 1, counter, dyn_stations);
         else
-            Choosestation_enter_menu(3, counter, dyn_stations);
+            Choosestation_enter_menu(counter, counter, dyn_stations);
     if (choice == DOWN)
-        if (switcher != 3)
+        if (switcher != counter)
             Choosestation_enter_menu(switcher + 1, counter, dyn_stations);
         else
             Choosestation_enter_menu(1, counter, dyn_stations);
@@ -83,7 +81,7 @@ int Choosestation_enter(int switcher)
 	int i = 0;
 	char choize;
 	int razmer = 0;
-	char dyn_stations[10][30];
+	char dyn_stations[15][30];
 	int counter = 0, funk;
 	
 	cout<<"Enter name of stantion: ";
@@ -126,7 +124,7 @@ int Choosestation_enter(int switcher)
 	funk = Choosestation_enter_menu(1, counter, dyn_stations);
 	return funk;
     
-	for(int i =0; i<10; i++)
+	/*for(int i =0; i<20; i++)
 	{
 		strncpy(dyn_stations[i], stations[i], strlen(stations[i]));
 		for(int j =0; j<strlen(stations[i]); j++) cout<<stations[i][j];
@@ -138,7 +136,7 @@ int Choosestation_enter(int switcher)
 		{
 			dyn_stations[i][j] = '\0';
 			cout<<dyn_stations[i][j];
-		}  	
+		}  	*/
 }
 
 int main()
